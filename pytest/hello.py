5c8290eb5e1265767db854f3471616e8a2335943
wtnb99 <satoshic.watanabe@gmail.com>
2021-11-13 21:03:29 +0900

    Added pattern matching.

--- pytest/hello.py
+++ pytest/hello.py
@@ -1,6 +1,15 @@
+import re
+
 
 def say_hello(person="Taro"):
     return f"Hello, {person}"
+
+
+def extract_pattern(text):
+    pattern = r"\S (PASS|FAIL) (S.*)$"
+    r = re.search(pattern, text)
+    if r:
+        return r.group(1), r.group(2)
 
 
 def main():
--- pytest/test_hello.py
+++ pytest/test_hello.py
@@ -1,9 +1,21 @@
 import pytest
 
 from hello import say_hello
+from hello import extract_pattern
 
 
 def test_say_hello():
     assert say_hello("Taro") == "Hello, Taro"
     assert say_hello("Jiro") == "Hello, Jiro"
-    assert say_hello() != "Hello, Taro"
+    assert say_hello() == "Hello, Taro"
+
+
+def test_extract_pattern():
+    text = "Z PASS S001"
+    assert extract_pattern(text) == ("PASS", "S001")
+    text = "m FAIL S001"
+    assert extract_pattern(text) == ("FAIL", "S001")
+    text = "] PASS S002"
+    assert extract_pattern(text) == ("PASS", "S002")
+    text = "\\ FAIL S002"
+    assert extract_pattern(text) == ("FAIL", "S002")
